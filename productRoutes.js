65b17e7d33c65bb9c70ac132656f41304e831b43
Lorenz Allen <lorenz_allen_biscocho@dlsl.edu.ph>
2026-05-07 09:41:53 +0800

    Update productRoutes.js

--- productRoutes.js
+++ productRoutes.js
@@ -1,15 +1,11 @@
 const express = require("express");
-
-const productController = require("../controllers/productController");
+const productController = require("./productController");
 
 const router = express.Router();
 
 router.post("/", productController.createProduct);
-
 router.get("/", productController.getProducts);
-
 router.patch("/:id", productController.updateProduct);
-
 router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
